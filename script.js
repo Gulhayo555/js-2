99d26eef9f19b87076a8407e580b2cf234a0e98e
test <test>
2021-11-11 17:13:59 +0500

    JS

---
+++ script.js
@@ -0,0 +1,25 @@
+  var yosh= + prompt('yoshingizni kiriting')
+if (0 < 18 ){
+   alert('yoshsiz o"qishingiz kerak')
+}
+
+ if(18 < 50){
+    alert('ishlashingiz kerak')
+ }
+
+
+ if (  50 < 59 ){
+    alert('yaqinda pensiyaga chiqasiz') 
+}
+
+
+if (59<150) {
+    alert('pensionerga oxshaysiz');
+   
+}
+alert('Nimadir notog"ri ketib qoldi');
+
+var num2= +prompt('Birinchi sonni kiriting')
+var num4= +prompt('Ikkinchi  sonni kiriting')
+var num6= +prompt('Uchinchi sonni kiriting')
+alert('orta qiymat 4')
